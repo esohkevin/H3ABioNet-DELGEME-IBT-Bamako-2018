523aa6b12f0fc59f1ba9a4d06159bb6fb9f70e0a
esohkevin <esohkevin4@gmail.com>
2018-10-04 20:35:57 +0000

    update Setup.sh

--- H3A-IBT-Project/Setup.sh
+++ H3A-IBT-Project/Setup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # Create a directory in your home directory to install the application in
-
+filed="H3A-IBT-2018"
 createH3A() {
-
-f="H3A-IBT-2018"	
 for f
-     do [ -e "$f" ] && mkdir -p $f $HOME/
+     do 
+	[ -e "$f" ] && mkdir -p "$f" $HOME/
 done
 }
-createH3A $f
+createH3A $filed
 
-# Place the function in your home directory where bash can find it easily
-mv .myfunctions $HOME
+# Place the function in your home directory where bash can find it easily.
+myfunc=".myfunctions"
+mvFunc() {
+for func
+do
+	[ -e "$func" ] && mv "$func" $HOME
+
+done
+}
+mvFunc $myfunc
 
 # Add a line of code into you bashrc file to have bash always activate your
 # function once bash is started interactively
