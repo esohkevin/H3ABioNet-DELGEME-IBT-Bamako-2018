2a1599e0d9b4bb0fa2a4c70852ef3c6763d7e48c
Kevin Esoh <esohkevin4@gmail.com>
2018-10-04 15:15:12 +0000

    Update Setup.sh

--- H3A-IBT-Project/Setup.sh
+++ H3A-IBT-Project/Setup.sh
@@ -8,6 +8,8 @@ f="H3A-IBT-2018"
 for f
      do [ -e "$f" ] && mkdir -p $HOME/$f
 done
+}
+createH3BA $f
 
 # Place the function in your home directory where bash can find it easily
 mv .myfunctions > $HOME
@@ -16,5 +18,3 @@ mv .myfunctions > $HOME
 # function once bash is started interactively
 echo "source $HOME/.myfunctions" >> $HOME/.bashrc
 
-}
-createH3BA $f
