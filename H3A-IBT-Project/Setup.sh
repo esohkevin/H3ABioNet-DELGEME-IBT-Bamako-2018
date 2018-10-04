e0d6b4ad3bc999666d8ffcbbfab7aec8b8d3d1d0
esohkevin <esohkevin4@gmail.com>
2018-10-04 13:34:47 +0000

    added setup file and modified .gitignore file

---
+++ H3A-IBT-Project/Setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create a directory in your home directory to install the application in
+
+createH3BA() {
+
+f="H3A-IBT-2018"	
+for f
+     do [ -e "$f" ] && mkdir -p $HOME/$f
+done
+
+# Place the function in your home directory where bash can find it easily
+mv .myfunctions > $HOME
+
+# Add a line of code into you bashrc file to have bash always activate your
+# function once bash is started interactively
+echo "source $HOME/.myfunctions" >> $HOME/.bashrc
+
+createH3BA $f
+
+}
+
